333f0274017046e58f6023800110e8a5d8c04a59
Abdullah Munshi <stackbind@gmail.com>
2023-12-30 17:08:02 +0600

    Vue App Starter

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-red-800">{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Vue Basic Starter",
+    };
+  },
+};
+</script>
+
+<style></style>
